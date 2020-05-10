123e67f45fda61c0693850cbae15d6c6337f9e23
Otto <otto@masterbranch.io>
2020-05-10 11:07:06 +0100

    forcing everything to upper case when parsing

--- parsers/input_parser.py
+++ parsers/input_parser.py
@@ -2,16 +2,16 @@ from data_objects import Grid, Rover, RoverSetup, Mars
 
 
 def parse(commands):
-    x, y = commands[0].split()
+    x, y = commands[0].upper().split()
     grid = Grid(int(x), int(y))
 
     rover = None
     rover_setups = []
     for idx, command in enumerate(commands[1:]):
         if idx % 2 == 0:
-            rover_x, rover_y, direction = command.split()
+            rover_x, rover_y, direction = command.upper().split()
             rover = Rover(int(rover_x), int(rover_y), direction)
         else:
-            rover_setups.append(RoverSetup(rover, [char for char in command]))
+            rover_setups.append(RoverSetup(rover, [char for char in command.upper()]))
 
     return Mars(grid, rover_setups)
